4d2e4db03f4fac52b574df8e73b33268ccaf6525
prashanta <pbt9840@gmail.com>
2023-01-14 01:10:42 +0545

    all error managed in user controller

--- controller/userController.js
+++ controller/userController.js
@@ -11,16 +11,16 @@ const getAllUsers = async (req,res,next) =>{
       });
     } 
     catch (err) {
-        next(nextHandlers("Users fetching faild",err))
+        next(nextHandlers("Users fetching failed",err))
       }
     }
 const getUserById = async (req,res,next) =>{
     const {id} = req.params
     try {
-      const getUserById = await UsersModel.findById(id)
+      const user = await UsersModel.findById(id)
       res.status(200).json({
-        message: "Users found successfully",
-        data: getUserById,
+        message: "User found successfully",
+        data: user,
       });
     } 
     catch (err) {
@@ -28,11 +28,11 @@ const getUserById = async (req,res,next) =>{
       }
     }
 const createUser = async (req,res,next) =>{
-    const Users = new UsersModel(req.body);
+    const user = new UsersModel(req.body);
     try {
-      const savedUsers = await Users.save();
+      const savedUsers = await user.save();
       res.status(200).json({
-        message: "Users created successfully",
+        message: "User created successfully",
         data: savedUsers,
       });
     } 
@@ -40,7 +40,7 @@ const createUser = async (req,res,next) =>{
       if (err.code === 11000) {
         next(nextHandlers("duplicate data already exist",err))
       } else {
-        next(nextHandlers("Users creation failed",err));
+        next(nextHandlers("User creation failed",err));
       }
     }
 }
@@ -49,12 +49,12 @@ const updateUser = async (req,res,next) =>{
     try {
       const updatedUsers = await UsersModel.findByIdAndUpdate(id,{$set:req.body},{new:true})
       res.status(200).json({
-        message: "Users updated successfully",
+        message: "User updated successfully",
         data: updatedUsers,
       });
     } 
     catch (err) {
-        next(nextHandlers("Users update failed!!",err))
+        next(nextHandlers("User update failed!!",err))
       }
     }
 
@@ -63,12 +63,12 @@ const replaceUser = async (req,res,next) =>{
     try {
       const replacedUsers = await UsersModel.findOneAndReplace(id,{$set:req.body},{new:true})
       res.status(200).json({
-        message: "Users replaced successfully",
+        message: "User replaced successfully",
         data: replacedUsers,
       });
     } 
     catch (err) {
-        next(nextHandlers("Users replacing failed!!",err))
+        next(nextHandlers("User replacing failed!!",err))
       }
     }
 const deleteUser = async (req,res,next) =>{
@@ -76,12 +76,12 @@ const deleteUser = async (req,res,next) =>{
     try {
       const deletedUsers = await UsersModel.findByIdAndDelete(id,{$set:req.body})
       res.status(200).json({
-        message: "Users deleted successfully",
+        message: "User deleted successfully",
         data: deletedUsers,
       });
     } 
     catch (err) {
-        next(nextHandlers("Users deletion failed!!",err))
+        next(nextHandlers("User deletion failed!!",err))
       }
     }
 
